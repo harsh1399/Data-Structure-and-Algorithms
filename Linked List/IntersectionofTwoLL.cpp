b7116989833cdd43a7646bb5212f50573a8a710d
harsh1399 <harsh321mahajan@gmail.com>
2022-12-25 10:46:13 -0700

    intersection of LL

--- Linked List/IntersectionofTwoLL.cpp
+++ Linked List/IntersectionofTwoLL.cpp
@@ -45,6 +45,25 @@ int findNode(Node *head1,Node *head2)
         return findIntersection(head2,head1,diff);
     }
 }
+// Another method
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode *temp1 = headA, *temp2 = headB;
+//         while(temp1!=temp2){
+//             if(temp1==nullptr)
+//                 temp1 = headB;
+//             else
+//                 temp1 = temp1->next;
+//             if(temp2 == nullptr)
+//                 temp2 = headA;
+//             else
+//                 temp2 = temp2->next;
+//         }
+//         return temp1;
+//     }
+// };
+
 int main()
 {
     LinkedList list1,list2;
